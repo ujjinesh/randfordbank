803e1f1a882eaab7f17adf32c360182ccf1e8643
bandi ujjinesh <ujjines@gmail.com>
2023-09-04 14:52:53 +0530

    keyword driven framework for primus bank

---
+++ src/com/primusbank/tests/BankerLoginTest.java
@@ -0,0 +1,28 @@
+package com.primusbank.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.primusbank.constants.PrimusBankConstants;
+import com.primusbank.library.BankerHomePage;
+import com.primusbank.library.PrimusBankHomePage;
+
+public class BankerLoginTest extends PrimusBankConstants 
+{
+	@Test
+	public void bankerLoginTest()
+	{
+		
+		boolean res;
+		PrimusBankHomePage phome=new PrimusBankHomePage();
+		phome.branchname="Ameerpet";
+		phome.bankeruid="Richards";
+		phome.bankerpwd="demo";
+		res=phome.bankerLogin(phome.branchname, phome.bankeruid, phome.bankerpwd);
+		Assert.assertTrue(res);		
+		BankerHomePage bhome=new BankerHomePage();
+		bhome.bankerLogout();  
+		
+	}
+	
+}
---
+++ src/snippet/Snippet.java
@@ -0,0 +1,12 @@
+package snippet;
+
+import javax.swing.text.Style;
+
+import org.apache.poi.ss.usermodel.CellStyle;
+
+public class Snippet {
+	public static void main(String[] args) {
+		//Style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+	}
+}
+
